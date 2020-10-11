22309b4ac60282516c814b9c84d53e6b0d52003c
Tanner Cabaniss <71537667+caba0009@users.noreply.github.com>
2020-10-11 12:04:19 -0500

    added "fire" data to package
    added the information in FIREDAM.csv into package under fire.r

---
+++ R/fire.R
@@ -0,0 +1,12 @@
+#' Damage caused by fire
+#'
+#' A dataset containing the damage in thousands of US dollars for a given distance from fire
+#' station in miles
+#'
+#' @format A data frame with 15 rows and 2 variables:
+#' \describe{
+#'   \item{DISTANCE}{distance from fire station in miles}
+#'   \item{DAMAGE}{x 1000 dollars in damage}
+#'}
+#'@source \url{https://www.crcpress.com/Statistics-for-Engineering-and-the-Sciences/Mendenhall-Sincich/p/book/9781498728850}
+"fire"
